8b0bf87c4b7da69887fe688418a263150726a294
Paviel Barysevich-Bleyaniuk <pavelmessage@gmail.com>
2024-07-10 17:47:12 +0300

    fix background

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let checkbox = document.getElementById('checkbox');
-let wrappers = document.getElementsByClassName('wrapper');
+let body = document.body;
 let containers = document.getElementsByClassName('container');
 
 checkbox.addEventListener('change', switchMode);
 
 function switchMode() {
-    for (let wrapper of wrappers) {
-        wrapper.classList.toggle('dark');
-    }
+        body.classList.toggle('dark');
 
     for (let container of containers) {
         container.classList.toggle('dark');
